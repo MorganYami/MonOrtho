a867ebb1ee9f678f9bdd8e52f8c2aa8b1007cc56
laban.guillaume.07@gmail.com <laban.guillaume.07@gmail.com>
2018-11-15 16:08:59 +0100

    Commit n°2 | 15/11/18

--- client/src/app/services/settings.ts
+++ client/src/app/services/settings.ts
@@ -1,6 +1,6 @@
 export namespace Settings {
 
     // export const urlIp = "http://192.168.2.70:3000";
-    export const urlIp = "localhost:3000";
+    export const urlIp = "http://localhost:3000";
 
 }
